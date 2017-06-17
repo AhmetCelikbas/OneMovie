087b6ab3511d5a7ea09b731317808386fe96b5df
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-06-17 19:11:10 +0200

    Add router loading progress bar

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+import { MdProgressBarModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
@@ -18,7 +20,8 @@ import 'hammerjs';
     BrowserAnimationsModule,
     FormsModule,
     HttpModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MdProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
