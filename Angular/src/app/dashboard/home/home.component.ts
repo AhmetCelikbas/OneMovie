8907ec916c9247ec28c48f7f50ad253331b32a9f
Ahmet Celikbas <celikbas.ahmet@gmail.com>
2017-06-18 15:31:34 +0200

    Update home page with now playing data from tmdb with hammerJS to swipe pages

--- Angular/src/app/dashboard/home/home.component.ts
+++ Angular/src/app/dashboard/home/home.component.ts
@@ -1,15 +1,51 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../../services/data.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+    movies: Object;
+    currentPage: number;
+    totalPages: number;
 
-  constructor() { }
+    SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight' };
 
-  ngOnInit() {
-  }
+    constructor(private data: DataService) {
+        this.currentPage = 1;
+        this.totalPages = 1;
+    }
+
+    ngOnInit() {
+        this.data.getNowPlaying(this.currentPage).subscribe(response => {
+            this.totalPages = response['total_pages'];
+            this.movies = response;
+        });
+    }
+
+    setPage(page: number) {
+        if (page > this.totalPages || page < 1) {
+            return; // out of range
+        }
+        this.movies = null;
+        this.currentPage = page;
+        this.data.getNowPlaying(this.currentPage).subscribe(response => this.movies = response);
+    }
+
+    // action triggered when user swipes
+    swipe(action = this.SWIPE_ACTION.RIGHT) {
+        // swipe right, next 
+        if (action === this.SWIPE_ACTION.RIGHT) {
+            this.setPage(this.currentPage - 1);
+        }
+
+        // swipe left, previous avatar
+        if (action === this.SWIPE_ACTION.LEFT) {
+            this.setPage(this.currentPage + 1);
+        }
+    }
 
 }
